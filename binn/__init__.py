7c6c35132a2d7242fdd443fd843a6dfb509fa72f
ErikHartman <erik.hartman@hotmail.com>
2023-01-30 11:45:14 +0100

    added ImportanceNetwork

--- binn/__init__.py
+++ binn/__init__.py
@@ -1,6 +1,6 @@
 from .binn import BINN
 from .logger import SuperLogger
-from .network import Network
+from .network import Network, ImportanceNetwork
 from .sklearn import BINNClassifier
 from .plot import *
 from .shap import BINNExplainer
