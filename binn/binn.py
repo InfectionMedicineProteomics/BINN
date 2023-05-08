e42a528c0b9d750919617e7f036d8146bbd41b69
ErikHartman <erik.hartman@hotmail.com>
2023-05-08 13:07:14 +0200

    update

--- binn/binn.py
+++ binn/binn.py
@@ -10,10 +10,10 @@ from binn.network import Network
 
 class BINN(LightningModule):
     """
-    Implements a Biologically Informed Neural Network (BINN). The BINN 
+    Implements a Biologically Informed Neural Network (BINN). The BINN
     is implemented using the PyTorch Lightning-framework.
-    If you are unfamiliar with PyTorch, we suggest visiting 
-    their website: https://pytorch.org/ 
+    If you are unfamiliar with PyTorch, we suggest visiting
+    their website: https://pytorch.org/
 
 
     Args:
@@ -57,14 +57,12 @@ class BINN(LightningModule):
         dropout: float = 0,
         residual: bool = False,
     ):
-
         super().__init__()
         self.residual = residual
         self.network = network
         self.n_layers = n_layers
 
-        connectivity_matrices = self.network.get_connectivity_matrices(
-            n_layers)
+        connectivity_matrices = self.network.get_connectivity_matrices(n_layers)
         layer_sizes = []
         self.layer_names = []
 
@@ -73,7 +71,9 @@ class BINN(LightningModule):
         layer_sizes.append(i)
         self.layer_names.append(matrix.index.tolist())
         self.features = matrix.index
+        self.trainable_params = matrix.to_numpy().sum()
         for matrix in connectivity_matrices[1:]:
+            self.trainable_params += matrix.to_numpy().sum()
             i, _ = matrix.shape
             layer_sizes.append(i)
             self.layer_names.append(matrix.index.tolist())
@@ -96,6 +96,7 @@ class BINN(LightningModule):
                 dropout=dropout,
             )
         self.apply(_init_weights)
+        self.weight = weight
         self.loss = nn.CrossEntropyLoss(weight=weight)
         self.learning_rate = learning_rate
         self.scheduler = scheduler
@@ -134,10 +135,8 @@ class BINN(LightningModule):
         loss = self.loss(y_hat, y)
         prediction = torch.argmax(y_hat, dim=1)
         accuracy = self.calculate_accuracy(y, prediction)
-        self.log("train_loss", loss, prog_bar=True,
-                 on_step=False, on_epoch=True)
-        self.log("train_acc", accuracy, prog_bar=True,
-                 on_step=False, on_epoch=True)
+        self.log("train_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("train_acc", accuracy, prog_bar=True, on_step=False, on_epoch=True)
         return loss
 
     def validation_step(self, batch, _):
@@ -155,8 +154,7 @@ class BINN(LightningModule):
         prediction = torch.argmax(y_hat, dim=1)
         accuracy = self.calculate_accuracy(y, prediction)
         self.log("val_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
-        self.log("val_acc", accuracy, prog_bar=True,
-                 on_step=False, on_epoch=True)
+        self.log("val_acc", accuracy, prog_bar=True, on_step=False, on_epoch=True)
         return {"val_loss": loss, "val_acc": accuracy}
 
     def test_step(self, batch, _):
@@ -172,10 +170,8 @@ class BINN(LightningModule):
         loss = self.loss(y_hat, y)
         prediction = torch.argmax(y_hat, dim=1)
         accuracy = self.calculate_accuracy(y, prediction)
-        self.log("test_loss", loss, prog_bar=True,
-                 on_step=False, on_epoch=True)
-        self.log("test_acc", accuracy, prog_bar=True,
-                 on_step=False, on_epoch=True)
+        self.log("test_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("test_acc", accuracy, prog_bar=True, on_step=False, on_epoch=True)
 
     def configure_optimizers(self):
         """
@@ -283,14 +279,11 @@ def _generate_sequential(
     n_outputs=2,
     dropout=0,
 ):
-
     layers = []
     for n in range(len(layer_sizes) - 1):
         linear_layer = nn.Linear(layer_sizes[n], layer_sizes[n + 1], bias=bias)
         layers.append((f"Layer_{n}", linear_layer))  # linear layer
-        layers.append(
-            (f"BatchNorm_{n}", nn.BatchNorm1d(layer_sizes[n + 1]))
-        )
+        layers.append((f"BatchNorm_{n}", nn.BatchNorm1d(layer_sizes[n + 1])))
         if connectivity_matrices is not None:
             prune.custom_from_mask(
                 linear_layer,
@@ -305,9 +298,7 @@ def _generate_sequential(
             layers.append((f"Activation_{n}", activation[n]))
         else:
             _append_activation(layers, activation, n)
-    layers.append(
-        ("Output layer", nn.Linear(layer_sizes[-1], n_outputs, bias=bias))
-    )
+    layers.append(("Output layer", nn.Linear(layer_sizes[-1], n_outputs, bias=bias)))
     model = nn.Sequential(collections.OrderedDict(layers))
     return model
 
@@ -331,17 +322,14 @@ def _generate_residual(
             )
         layers.append((f"Dropout_{n}", nn.Dropout(0.2)))
         layers.append(
-            (f"Residual_out_{n}", nn.Linear(
-                layer_sizes[n + 1], n_outputs, bias=bias))
+            (f"Residual_out_{n}", nn.Linear(layer_sizes[n + 1], n_outputs, bias=bias))
         )
         layers.append((f"Residual_sigmoid_{n}", nn.Sigmoid()))
         return layers
 
     for res_index in range(len(layer_sizes)):
         if res_index == len(layer_sizes) - 1:
-            layers.append(
-                (f"BatchNorm_final", nn.BatchNorm1d(layer_sizes[-1]))
-            )
+            layers.append((f"BatchNorm_final", nn.BatchNorm1d(layer_sizes[-1])))
             layers.append((f"Dropout_final", nn.Dropout(0.2)))
             layers.append(
                 (
@@ -377,9 +365,7 @@ def _forward_residual(model: nn.Sequential, x):
         if name.startswith("Residual"):
             if "out" in name:
                 x_temp = layer(x)
-            if _is_activation(
-                layer
-            ):
+            if _is_activation(layer):
                 x_temp = layer(x_temp)
                 x_final = x_final + x_temp
                 residual_counter = residual_counter + 1
--- binn/feature_selection.py
+++ binn/feature_selection.py
@@ -7,10 +7,12 @@ import pandas as pd
 import pytorch_lightning
 
 
-class RecursivePathwayElimination():
-
+class RecursivePathwayElimination:
     def __init__(self, model: BINN, explainer: BINNExplainer):
         self.model = model
+        self.n_layers = model.n_layers
+        self.learning_rate = model.learning_rate
+        self.weight = model.weight
         self.network = model.network
         self.pathways = self.network.pathways
         self.mapping = self.network.unaltered_mapping
@@ -18,114 +20,194 @@ class RecursivePathwayElimination():
         self.explainer = explainer
         self.input_data = self.network.input_data
 
-    def fit(self, protein_matrix, design_matrix, nr_iterations: int = 20, max_epochs: int = 50,  clip_threshold=1e-5, constant_removal_rate=0.05, learning_rate=1e-2):
-        return_dict = {'models': [], 'val_acc': [], 'val_loss': [],
-                       'iteration': [], 'pathways': [], 'epochs': []}
-        fitted_protein_matrix = self._fit_data_matrix_to_network_input(
-            protein_matrix.reset_index(), features=self.network.inputs)
-        X_train, X_test, y_train, y_test = self._generate_data(fitted_protein_matrix,
-                                                               design_matrix=design_matrix)
-        train_dataloader = torch.utils.data.DataLoader(dataset=torch.utils.data.TensorDataset(torch.Tensor(X_train), torch.LongTensor(y_train)),
-                                                       batch_size=8,
-                                                       num_workers=12,
-                                                       shuffle=True)
-        val_dataloader = torch.utils.data.DataLoader(dataset=torch.utils.data.TensorDataset(torch.Tensor(X_test), torch.LongTensor(y_test)),
-                                                     batch_size=8,
-                                                     num_workers=12)
+    def fit(
+        self,
+        protein_matrix,
+        design_matrix,
+        nr_iterations: int = 20,
+        max_epochs: int = 50,
+        clip_threshold=1e-5,
+        constant_removal_rate=0.05,
+        min_features_per_layer=3,
+    ):
+        return_dict = {
+            "models": [],
+            "val_acc": [],
+            "val_loss": [],
+            "iteration": [],
+            "pathways": [],
+            "epochs": [],
+            "trainable_params": [],
+        }
+        self.fitted_protein_matrix = self._fit_data_matrix_to_network_input(
+            protein_matrix.reset_index(), features=self.network.inputs
+        )
+        X_train, X_test, y_train, y_test = self._generate_data(
+            self.fitted_protein_matrix, design_matrix=design_matrix
+        )
+        train_dataloader = torch.utils.data.DataLoader(
+            dataset=torch.utils.data.TensorDataset(
+                torch.Tensor(X_train), torch.LongTensor(y_train)
+            ),
+            batch_size=8,
+            num_workers=12,
+            shuffle=True,
+        )
+        val_dataloader = torch.utils.data.DataLoader(
+            dataset=torch.utils.data.TensorDataset(
+                torch.Tensor(X_test), torch.LongTensor(y_test)
+            ),
+            batch_size=8,
+            num_workers=12,
+        )
+        model = self.model
         for iteration in range(nr_iterations):
-            early_stopping = pytorch_lightning.callbacks.early_stopping.EarlyStopping(patience=10, min_delta=0.001,
-                                                                                      monitor="val_loss", mode="min")
+            self.model = model
+            total_removed_features = 0
+            pathway_dict = {}
+            early_stopping = pytorch_lightning.callbacks.early_stopping.EarlyStopping(
+                patience=10, min_delta=0.001, monitor="val_loss", mode="min"
+            )
             trainer = pytorch_lightning.Trainer(
-                max_epochs=max_epochs, log_every_n_steps=5, callbacks=[early_stopping])
+                max_epochs=max_epochs, log_every_n_steps=5, callbacks=[early_stopping]
+            )
             trainer.fit(self.model, train_dataloader, val_dataloader)
-            return_dict['epochs'].append(self.model.current_epoch)
+            return_dict["epochs"].append(self.model.current_epoch)
+            return_dict["trainable_params"].append(self.model.trainable_params)
             val_dict = trainer.validate(self.model, val_dataloader)
-            return_dict['models'].append(self.model)
-            return_dict['val_acc'].append(val_dict[0]['val_acc'])
-            return_dict['val_loss'].append(val_dict[0]['val_loss'])
-            return_dict['iteration'].append(iteration)
+            return_dict["models"].append(self.model)
+            return_dict["val_acc"].append(val_dict[0]["val_acc"])
+            return_dict["val_loss"].append(val_dict[0]["val_loss"])
+            return_dict["iteration"].append(iteration)
             explainer = BINNExplainer(self.model)
-            test_data = torch.Tensor(
-                np.concatenate([X_train, X_test], axis=0))
-            background_data = torch.Tensor(
-                np.concatenate([X_train, X_test], axis=0))
-            for wanted_layer in range(self.n_layers):
+            self.test_data = torch.Tensor(np.concatenate([X_train, X_test], axis=0))
+            self.background_data = torch.Tensor(
+                np.concatenate([X_train, X_test], axis=0)
+            )
+            for wanted_layer in range(self.n_layers + 1):
                 shap_dict = explainer._explain_layer(
-                    test_data, background_data, wanted_layer)
-                values = shap_dict['shap_values']
+                    self.test_data, self.background_data, wanted_layer
+                )
+                values = shap_dict["shap_values"]
                 values = np.abs(np.array(values))
                 values = np.mean(values, axis=1)
                 values = np.sum(values, axis=0)
-                shap_list = [(feature, value) for feature,
-                             value in zip(shap_dict['features'], values)]
-                shap_list = sorted(shap_list, key=lambda x: x[1])
-                features_below_clip = [x[0]
-                                       for x in shap_list if x[1] < clip_threshold]
-                n_features_below_clip = len(features_below_clip)
-                shap_list = shap_list[n_features_below_clip:]
-                total_n_elements = len(shap_list)
-                n_features_to_remove = int(
-                    total_n_elements*constant_removal_rate)
-                features_to_remove = [x[0]
-                                      for x in shap_list[:n_features_to_remove]]
+                shap_list = [
+                    (feature, value)
+                    for feature, value in zip(shap_dict["features"], values)
+                ]
+                if len(shap_list) <= min_features_per_layer:
+                    print(f"Min features per layer ({min_features_per_layer}) reached")
+                    pathway_dict[wanted_layer] = self.pathways.copy()
+                else:
+                    shap_list = sorted(shap_list, key=lambda x: x[1])
+                    features_below_clip = [
+                        x[0] for x in shap_list if x[1] < clip_threshold
+                    ]
+                    n_features_below_clip = len(features_below_clip)
+                    shap_list = shap_list[n_features_below_clip:]
+                    total_n_elements = len(shap_list)
+                    n_features_to_remove = int(total_n_elements * constant_removal_rate)
+                    features_to_remove = [
+                        x[0] for x in shap_list[:n_features_to_remove]
+                    ]
+                    features_to_remove += features_below_clip
+                    features_to_remove = list(set(features_to_remove))
+                    self.pathways = self.pathways[
+                        ~self.pathways["parent"].isin(features_to_remove)
+                    ]
+                    self.pathways = self.pathways[
+                        ~self.pathways["child"].isin(features_to_remove)
+                    ]
+                    pathway_dict[wanted_layer] = self.pathways.copy()
+                    print(f"Connections in layer {wanted_layer}: ", len(self.pathways))
+                    print(
+                        f"Removed pathways in layer {wanted_layer}: \
+                        {len(features_to_remove)} \
+                            out of {total_n_elements} \
+                        "
+                    )
+                    print(features_to_remove)
+                    total_removed_features += len(features_to_remove)
+            return_dict["pathways"].append(pathway_dict)
 
-                self.pathways = self.pathways[~self.pathways['parent'].isin(
-                    features_to_remove)]
-                self.pathways = self.pathways[~self.pathways['child'].isin(
-                    features_to_remove)]
-                return_dict['pathways'].append(self.pathways)
-                print(f'Pathways in layer {wanted_layer} ', len(self.pathways))
-                print(f'Removd features in layer {wanted_layer} ',
-                      n_features_below_clip + n_features_to_remove)
+            if total_removed_features == 0:
+                print(f"Stopped at iteration: {iteration}")
+                return return_dict
 
-            self.network = Network(input_data=self.input_data,
-                                   pathways=self.pathways, mapping=self.mapping)
+            self.network = Network(
+                input_data=self.input_data, pathways=self.pathways, mapping=self.mapping
+            )
 
-            self.model = BINN(
+            model = BINN(
                 network=self.network,
-                n_layers=4,
+                n_layers=self.n_layers,
                 dropout=0.2,
+                weight=self.weight,
                 validate=True,
                 residual=True,
-                scheduler='plateau',
-                learning_rate=learning_rate
+                scheduler="plateau",
+                learning_rate=self.learning_rate,
             )
-            fitted_protein_matrix = self._fit_data_matrix_to_network_input(
-                protein_matrix.reset_index(), features=self.network.inputs)
-            X_train, X_test, y_train, y_test = self._generate_data(fitted_protein_matrix,
-                                                                   design_matrix=design_matrix)
-            train_dataloader = torch.utils.data.DataLoader(dataset=torch.utils.data.TensorDataset(torch.Tensor(X_train), torch.LongTensor(y_train)),
-                                                           batch_size=8,
-                                                           num_workers=12,
-                                                           shuffle=True)
-            val_dataloader = torch.utils.data.DataLoader(dataset=torch.utils.data.TensorDataset(torch.Tensor(X_test), torch.LongTensor(y_test)),
-                                                         batch_size=8,
-                                                         num_workers=12)
+            self.fitted_protein_matrix = self._fit_data_matrix_to_network_input(
+                protein_matrix.reset_index(), features=self.network.inputs
+            )
+            X_train, X_test, y_train, y_test = self._generate_data(
+                self.fitted_protein_matrix, design_matrix=design_matrix
+            )
+            train_dataloader = torch.utils.data.DataLoader(
+                dataset=torch.utils.data.TensorDataset(
+                    torch.Tensor(X_train), torch.LongTensor(y_train)
+                ),
+                batch_size=8,
+                num_workers=12,
+                shuffle=True,
+            )
+            val_dataloader = torch.utils.data.DataLoader(
+                dataset=torch.utils.data.TensorDataset(
+                    torch.Tensor(X_test), torch.LongTensor(y_test)
+                ),
+                batch_size=8,
+                num_workers=12,
+            )
         return return_dict
 
-    def _fit_data_matrix_to_network_input(self, data_matrix: pd.DataFrame, features, feature_column="Protein") -> pd.DataFrame:
+    def get_final_model(self):
+        return self.model
+
+    def get_protein_matrix(self):
+        return self.fitted_protein_matrix
+
+    def _fit_data_matrix_to_network_input(
+        self, data_matrix: pd.DataFrame, features, feature_column="Protein"
+    ) -> pd.DataFrame:
         nr_features_in_matrix = len(data_matrix.index)
         if len(features) > nr_features_in_matrix:
             features_df = pd.DataFrame(features, columns=[feature_column])
-            data_matrix = data_matrix.merge(
-                features_df, how='right', on=feature_column)
+            data_matrix = data_matrix.merge(features_df, how="right", on=feature_column)
         if len(features) > 0:
             data_matrix.set_index(feature_column, inplace=True)
             data_matrix = data_matrix.loc[features]
         return data_matrix
 
-    def _generate_data(self, data_matrix: pd.DataFrame, design_matrix: pd.DataFrame, groups=[1, 2], test_size=0.25):
+    def _generate_data(
+        self,
+        data_matrix: pd.DataFrame,
+        design_matrix: pd.DataFrame,
+        groups=[1, 2],
+        test_size=0.25,
+    ):
         y = []
         dfs = []
         for i, group in enumerate(groups):
-            group_columns = design_matrix[design_matrix['group']
-                                          == group]['sample'].values
+            group_columns = design_matrix[design_matrix["group"] == group][
+                "sample"
+            ].values
             df = data_matrix[group_columns].T
             dfs.append(df)
             y += [i for _ in group_columns]
         y = np.array(y)
         X = pd.concat(dfs).fillna(0).to_numpy()
         X = preprocessing.StandardScaler().fit_transform(X)
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=test_size)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
         return X_train, X_test, y_train, y_test
