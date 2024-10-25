0de81fd8544073894fc980a603bda982f6749740
jmi <jonas.meirer@csem.ch>
2024-10-25 11:19:10 +0200

    bug fix and new test

--- tests/test_explainer.py
+++ tests/test_explainer.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 @pytest.fixture
-def binn_explainer():
+def binn_explainer(return_input=False):
     input_data = pd.DataFrame({"Protein": ["a", "b", "c"]})
     pathways = pd.DataFrame(
         {"source": ["A", "B", "C"], "target": ["path1", "path1", "path2"]}
@@ -20,7 +20,6 @@ def binn_explainer():
 
 def test_binn_explainer_init(binn_explainer: BINNExplainer):
     assert isinstance(binn_explainer.model, BINN)
-
 
 def test_binn_explainer_update_model(binn_explainer: BINNExplainer):
     input_data = pd.DataFrame({"Protein": ["a", "b", "c"]})
@@ -43,6 +42,26 @@ def test_binn_explainer_explain(binn_explainer: BINNExplainer):
     result = binn_explainer.explain(test_data, background_data)
     assert isinstance(result, pd.DataFrame)
     assert len(result) > 0
+    
+def test_binn_explainer_explain_output(binn_explainer: BINNExplainer):
+    test_data = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32)
+    background_data = torch.tensor([[7, 8, 9], [10, 11, 12]], dtype=torch.float32)
+
+    result = binn_explainer.explain(test_data, background_data)
+
+    unique_pairs = result[["source name", "target name"]].drop_duplicates()
+    unique_pairs = [(s, t) for s, t in zip(unique_pairs["source name"], unique_pairs["target name"])]
+    
+    theoretical_unique_pairs = []
+    for cm in binn_explainer.model.connectivity_matrices:
+        for i in cm.index.tolist():
+            for j in cm.columns.tolist():
+                if cm.loc[i, j] == 1:
+                    theoretical_unique_pairs.append((i, j))
+                    
+    print(unique_pairs)
+         
+    assert set(unique_pairs) == set(theoretical_unique_pairs)
 
 
 def test_binn_explainer_explain_input(binn_explainer: BINNExplainer):
