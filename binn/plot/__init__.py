4f874fef29b2409e67a6b5e4bf8bb345659bde92
ErikHartman <erik.hartman@hotmail.com>
2025-02-03 14:13:02 +0100

    Fix bug in init

--- binn/plot/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model.binn import BINN
-from .model.dataloader import BINNDataLoader
-from .model.trainer import BINNTrainer
-from .model.pathway_network import PathwayNetwork
-from .analysis.explainer import BINNExplainer
